f73f66efd89871ff2e2ea96bf6034f32b8d747ba
GauravSinghSamant <thorusamant8@gmail.com>
2024-10-23 17:42:07 +0530

    some more practice

--- viteapp/src/main.jsx
+++ viteapp/src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
+
 import App from './App.jsx'
 import Counter from './Counter.jsx'
 import Tailwind from './Tailwind.jsx'
 import BGcolor from './BGcolor.jsx'
 import Passwordgenerator from './Passwordgenerator.jsx'
-import './index.css'
+import CurrencyConverter from './CurrencyConverter.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Passwordgenerator />
+    <CurrencyConverter />
   </React.StrictMode>,
 )
