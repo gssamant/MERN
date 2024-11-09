913fc76ee1f6674dd0f99beacef8fd92883b0912
GauravSinghSamant <thorusamant8@gmail.com>
2024-11-09 17:58:42 +0530

    Functions for generating short URL and get analytics

---
+++ url-shortener/controllers/url.js
@@ -0,0 +1,24 @@
+const shortid = require("shortid");
+const URL = require("../models/url");
+
+async function handleGenerateNewShortURL(req, res) {
+  const body = req.body;
+  if (!body.url) return res.status(400).json({ error: "URL is required" });
+  const shortID = shortid();
+  await URL.create({
+    shortID: shortID,
+    redirectURL: body.url,
+    visitHistory: [],
+  });
+  return res.json({ id: shortID });
+}
+
+async function handleGetAnalytics(req, res) {
+	const shortID = req.params.shortID;
+	const result = await URL.findOne({ shortID });
+	return res.json({
+		totalClicks: result.visitHistory.length,
+		analytics: result.visitHistory,
+	})
+}
+module.exports = { handleGenerateNewShortURL, handleGetAnalytics };
